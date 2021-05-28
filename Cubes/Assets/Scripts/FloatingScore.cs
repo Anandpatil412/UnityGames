4d0918c446a39e4c5026fc08cef29129fe5d97d9
Anandpatil412 <anands412@gmail.com>
2021-05-28 09:43:16 +0530

    added fade font

--- Cubes/Assets/Scripts/FloatingScore.cs
+++ Cubes/Assets/Scripts/FloatingScore.cs
@@ -7,16 +7,20 @@ public class FloatingScore : MonoBehaviour
 {
     TextMeshPro text;
     public int newPoints;
+    public float fadeDuration = 2f;
+    public float speed = 2f;
 
     private void Awake()
     {
         text = GetComponent<TextMeshPro>();
-        StartCoroutine(SelfDestruct());
+        //StartCoroutine(SelfDestruct());
 
-        start = GetComponent<Transform>();
-        end = GetComponent<Transform>();
+        //start = GetComponent<Transform>();
+        //end = GetComponent<Transform>();
 
-        end.position = new Vector3(start.position.x, start.position.y, start.position.z);
+        //end.position = new Vector3(start.position.x, start.position.y, start.position.z);
+
+        StartCoroutine(Fade());
     }
 
     public void UpdateFloatingScore(int newPoints)
@@ -24,25 +28,27 @@ public class FloatingScore : MonoBehaviour
         text.SetText(""+ newPoints);
     }
 
-    IEnumerator SelfDestruct()
+    IEnumerator Fade()
     {
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject);
+        float speed = (float)1 / fadeDuration;
+        Color c = text.color;
+
+        for(float t = 0.0f; t < 1f; t += Time.deltaTime * speed)
+        {
+            c.a = Mathf.Lerp(1,0,t);
+            text.color = c;
+            yield return true;
+        }
+
+        Destroy(this.gameObject);
+
     }
-
-
-    float timeElapsed;
-    float lerpDuration = 10f;
-    Transform start;
-    Transform end;
-    float lerptime = 10f;
-
-    float valueToLerp;
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, end.position + new Vector3(0,0.2f,0), lerptime * Time.deltaTime);
-        transform.localScale -= new Vector3(0.02f, 0.02f, 0.2f);
+        //transform.position = Vector3.Lerp(transform.position, end.position + new Vector3(0,0.2f,0), lerptime * Time.deltaTime);
+        //transform.localScale -= new Vector3(0.02f, 0.02f, 0.2f);
+        this.transform.Translate(Vector3.up * Time.deltaTime * speed);
     }
 
 }
